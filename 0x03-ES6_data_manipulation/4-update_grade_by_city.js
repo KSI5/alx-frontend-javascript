60fc61077ce0cfbf2b6916c54294502e8bcec3d3
KSI5 <krissigebs@gmail.com>
2024-01-09 09:05:27 +0000

    Fixed 4-update_grade_by_city.js

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,13 +1,19 @@
-const updateStudentGradeByCity = (listStudents, city, newGrades) => {
-  return listStudents
-    .filter(student => student.location === city)
-    .map(student => {
-      const gradeInfo = newGrades.find(grade => grade.studentId === student.id);
+/* eslint-disable */
+function updateStudentGradeByCity(students, city, newGrades) {
+    const studentsInCity = students.filter((student) => student.location === city);
+    return studentsInCity.map((student) => {
+      const filteredGrades = newGrades.filter((grade) => grade.studentId === student.id);
+      if (filteredGrades.length > 0) {
+        return {
+          ...student,
+          grade: filteredGrades[0].grade,
+        };
+      }
       return {
         ...student,
-        grade: gradeInfo ? gradeInfo.grade : 'N/A'
+        grade: 'N/A',
       };
     });
-};
-
-export default updateStudentGradeByCity;
+  }
+  
+  export default updateStudentGradeByCity;
